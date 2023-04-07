50defae79d05102c26479351917999d0f7d4d0d1
LimjeongSik <ol37@naver.com>
2023-04-07 21:57:44 +0900

    Updates

---
+++ _next/static/4booDhWygPCxp3GL2tGTR/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/969-07f3b01880351d8a.js","static/chunks/pages/index-01579981598935c6.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
